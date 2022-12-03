6391928edd0098f11e474b9dbafd26b773fad739
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2022-12-03 01:05:15 -0500

    generate strings within a range

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-import hashlib
+import string
+from hashlib import md5
 
 app = Flask(__name__)
 
@@ -11,9 +12,42 @@ def index():
 def crack():
     md5 = request.args.get('md5')
     # hash = hashlib.md5('123'.encode()).hexdigest()
-    
+
     return render_template('crack.html', md5=md5)
 
-def work_generator():
-    raise NotImplementedError
+def str2nums(s):
+    nums = []
+    for c in s:
+        if c.islower():  # 0-25 is a-z
+            nums.append(ord(c) - ord('a'))
+        else:  # 26-51 is A-Z
+            nums.append(ord(c) + 26 - ord('A'))
+    return nums
 
+def nums2str(nums):
+    str_builder = []
+    for n in nums:
+        if n < 26:  # 0-25 is a-z
+            str_builder.append(chr(n + ord('a')))
+        else:  # 26-51 is A-Z
+            str_builder.append(chr(n - 26 + ord('A')))
+    return ''.join(str_builder)
+
+# A-Z: 65-90, a-z: 97-122
+def str_generator(start_s, end_s):
+    # assuming start_s < end_s and are valid strings
+    start_nums, end_nums = str2nums(start_s), str2nums(end_s)
+    nums = start_nums
+    while nums <= end_nums:
+        yield nums2str(nums)
+
+        # increment by 1
+        i = len(nums) - 1
+        while i >= 0:
+            nums[i] = nums[i] + 1
+            if nums[i] < 52:
+                break
+            nums[i] %= 52
+            i -= 1
+        if i < 0:
+            return
