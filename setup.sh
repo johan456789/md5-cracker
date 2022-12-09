05ced591fe74c4c89dabc92c2fa5aba00be7f0bd
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2022-12-09 14:07:28 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,2 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
-echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-conda init
-echo 'conda activate web && cd ~/src' >> ~/.bashrc
