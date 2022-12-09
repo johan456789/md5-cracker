70403b704d76a2fb8492953564d8ed4be88e644c
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2022-12-09 12:48:01 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
-# echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
-# source ~/.bashrc
+echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
 cd src
 conda create -n web python pip flask tqdm -y
 # conda init && exec bash
