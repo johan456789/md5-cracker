25d4f99eef8ec7537384b0ad4d961157358a92e6
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2022-12-09 14:23:30 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
+$HOME/miniconda/bin/conda init
+$HOME/miniconda/bin/conda create -n web python pip flask tqdm -y
+echo 'conda activate web && cd ~/src' >> ~/.bashrc
