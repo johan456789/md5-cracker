ba4e3efca01a474bde5367fbf7083250f1d47db9
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2022-12-09 13:00:08 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,36 @@
+# wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh -O ~/miniconda.sh
+# bash ~/miniconda.sh -b -p $HOME/miniconda
+# echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
+# source ~/.bashrc
+# cd src
+# conda create -n web python pip flask tqdm -y
+# # conda init && exec bash
+# # echo 'conda activate web && cd ~/src' >> ~/.bashrc
+# # conda activate web
+# # pip install -r requirements.txt
+
+
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
-cd src
 conda create -n web python pip flask tqdm -y
-# conda init && exec bash
+conda init && exec bash
+echo 'conda activate web && cd ~/src' >> ~/.bashrc
+conda activate web
+pip install -r requirements.txt
+
+# # wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh -O ~/miniconda.sh
+# # bash ~/miniconda.sh -b -p $HOME/miniconda
+# # cd src
+# # conda create -n web python pip flask tqdm -y
 # echo 'conda activate web && cd ~/src' >> ~/.bashrc
-# conda activate web
-# pip install -r requirements.txt
-
-
+# # conda activate web
 # wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh -O ~/miniconda.sh
 # bash ~/miniconda.sh -b -p $HOME/miniconda
-# cd src
-# conda create -n web python pip flask tqdm -y
-echo 'conda activate web && cd ~/src' >> ~/.bashrc
+# export PATH="$HOME/miniconda/bin:$PATH"
+# git clone https://github.com/johan456789/md5-cracker/ && cd src
+# conda create -n web python pip -y
+# conda init && exec bash
 # conda activate web
+# pip install -r requirements.txt
