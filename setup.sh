15eb0e90f3f11e208d7323797d4ec125c621df13
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2022-12-09 11:32:12 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh 
 bash ~/miniconda.sh -b -p $HOME/miniconda
 echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
-git clone https://github.com/johan456789/md5-cracker/ && cd src
+cd src
 conda create -n web python pip -y
 conda init && exec bash
 echo 'conda activate web && cd ~/src' >> ~/.bashrc
